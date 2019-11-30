13caf080cfb88afdec662a3bf3dcdb41b2788dda
Adali12 <abayiringira1994@yahoo.com>
2019-11-30 13:34:54 -0800

    profile creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,3 +33,29 @@ end
       end
     end
   end
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was complteted updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was complteted destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+    def user_params
+      params.require(:user).permit(:firstName, :lastName, :email, :address, :bio, :occupation, :expertise, :admin, :mentor, :password, :password_confirmation)
+    end
+end
